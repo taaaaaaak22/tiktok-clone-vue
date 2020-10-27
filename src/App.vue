d2528f775681dc3d07808887154b57f3281fd1ea
taaaaaaak22 <takahi1210@gmail.com>
2020-10-28 05:28:41 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Video />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Video from './components/Containers/Video.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Video,
+  },
+})
+</script>
